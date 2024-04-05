d1772529d499408f9116937ccc5aa0da0af23edc
Fac3Hunter <shevchenko.tech@gmail.com>
2024-04-05 19:39:38 +0300

    initial state, most dependencies installed, yt skeleton cubes are made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import YouTubeButton from "./components/YouTubeButton";
+import { useStore } from "./store";
 
 function App() {
+  const { bears, increasePopulation } = useStore();
+
+  useEffect(() => {
+    document.body.classList.add("bg-background");
+    return () => {
+      document.body.classList.remove("bg-background");
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center bg-[#0f0f0f]">
+      <div className="flex flex-wrap justify-evenly items-center h-screen w-[80%] ">
+        <YouTubeButton />
+        <YouTubeButton />
+        <YouTubeButton />
+        <YouTubeButton />
+        <YouTubeButton />
+        <YouTubeButton />
+        <YouTubeButton />
+        <YouTubeButton />
+      </div>
     </div>
   );
 }
---
+++ src/components/YouTubeButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const YouTubeButton = () => {
+  return (
+    <div className="flex justify-center items-center text-3xl font-bold underline h-[200px] w-[350px]  rounded-xl bg-[#535353] hover:bg-slate-100 transition-colors"></div>
+  );
+};
+
+export default YouTubeButton;
